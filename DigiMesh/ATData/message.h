31de2ad3a6bb4b5632d060f46ba7a70a4f651b44
majorpr13 <kkroeger@araustech.com>
2018-06-24 17:42:37 -0400

    Extra Functionality
    Becuase of the Programmable Radio set it was easier to flash existing hardware with a simpler program and support an extra receive data layer than it is to generate an additional set of firmware that modifies the internal AT command post bootloader of the XBEEs. It appears that this message may also contain some interesting information. This should help keep consistency of radios and not have to worry about types received.

--- DigiMesh/ATData/message.h
+++ DigiMesh/ATData/message.h
@@ -11,9 +11,12 @@ class Message : public IATData {
 
 public:
 
-    Message(const std::vector<uint8_t> &data)
+    Message(const std::vector<uint8_t> &data, const bool &explicitFrame = false)
     {
-        DeSerialize(data);
+        if(!explicitFrame)
+            DeSerialize(data);
+        else
+            DeSerializeExplicit(data);
     }
 
     uint64_t addr;
@@ -35,6 +38,20 @@ private:
             data.push_back(msg.at(i));
         }
     }
+
+    virtual void DeSerializeExplicit(const std::vector<uint8_t> &msg){
+        addr = 0;
+        for(int i = 0 ; i < 8 ; i++) {
+            addr |= (((uint64_t)msg[1+i]) << (8*(7-i)));
+        }
+
+        broadcast = msg[17] == 0x02;
+
+        for(size_t i = 18 ; i < msg.size() ; i++) {
+            data.push_back(msg.at(i));
+        }
+    }
+
 
     virtual std::vector<uint8_t> Serialize() const {
         throw "Output Only Data";
